6e28774a5d57a21afa140f6e5eaad37707e80121
Ziad <zezo@gmail.com>
2022-08-31 16:40:21 +0300

    Add

---
+++ Embedded C/U3L1_Embedded C/Code/U3L1_Embedded C.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : U3L1_Embedded.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "Typedef.h"
+
+int main() {
+
+printf("%d",s);
+
+	return 0;
+}
